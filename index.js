96800274b51b254808dc17c5613dc373ba9baf90
NathanielAbalos <143874233+NathanielAbalos@users.noreply.github.com>
2024-02-16 11:40:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const announcementDivBox = document.querySelector(".announcementContentBox");
     const accessToken = "EAAXqFT6ABREBO7xhk1OY9OA07XDFUXLxHMKV2vupcpMhYPuz7MZBfVOaRt8M2rKDR4Br5trvj8Hhn2ZBe2JoqFl9qjQC1UZBEpUDZBopSkq6xUoBgksEdVhSSZCLcy4P2atKAVCHUb75EdB3O28uM67aL3nwf4nBGXydEarl41NCSd0uogNX6pgrA";
     const pageID="353855911379546";
-    const endPoint = `https://graph.facebook.com/v18.0/353855911379546/feed?access_token=${accessToken}`;
+    const endPoint = `https://graph.facebook.com/v18.0/353855911379546/feed?limit=5&access_token=${accessToken}`;
     let date = new Date();   
     let updatesCollection=[];
     let updateImages=["update3.jpg", "update4.jpg", "update7.jpg", "ctuScholarshipbg2.jpg", "ctuBuildingbg3.jpg", "ctuMainbg.jpeg","office2.jpg", "ctuGym.jpg"] ;
