a3baa98ffb08d3a5eb97c60035722608c6294fb2
NathanielAbalos <143874233+NathanielAbalos@users.noreply.github.com>
2024-03-05 14:16:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     const announcementDivBox = document.querySelector(".announcementContentBox");
-    const accessToken = "EAAXqFT6ABREBO7xhk1OY9OA07XDFUXLxHMKV2vupcpMhYPuz7MZBfVOaRt8M2rKDR4Br5trvj8Hhn2ZBe2JoqFl9qjQC1UZBEpUDZBopSkq6xUoBgksEdVhSSZCLcy4P2atKAVCHUb75EdB3O28uM67aL3nwf4nBGXydEarl41NCSd0uogNX6pgrA";
+    const accessToken = "EAAXqFT6ABREBO7ZCkoHUiDkFf3MPZByAIk0Rp99IXkNu163oH2iCL8dAZAfqlD8Xn8wlGUSa9PxCtMBSZBka6rehN5KQORsUZAeGZAtBagnKOBIKZCF2ZBkmzYVbpcG2ButqXEZAYUqCZA50ayyvMcxKN6PDfpz9tqetz7SigJSwc3ka1G0teGZAPCMSKf9";
     const pageID="353855911379546";
     const endPoint = `https://graph.facebook.com/v18.0/353855911379546/feed?limit=5&access_token=${accessToken}`;
     let date = new Date();   
